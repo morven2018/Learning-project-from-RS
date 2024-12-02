a18af1c1d81c5cd2d368d6135c9c1da0a040aeef
Alena Pudina <morven2018@yandex.ru>
2024-12-02 07:52:09 +0300

    feat: add burger menu

--- christmas-shop/scripts/burger.js
+++ christmas-shop/scripts/burger.js
@@ -1,4 +1,14 @@
-var wrapperMenu = document.querySelector("#menu");
+const wrapperMenu = document.getElementById("menu");
 wrapperMenu.addEventListener("click", function () {
     wrapperMenu.classList.toggle("open");
+    const navMenu = document.querySelector(".menu");
+    navMenu.classList.toggle('open_nav');
+    navMenu.style.width = document.documentElement.clientWidth;
+    const navLinks = Array.from(document.querySelector('.nav-link'));
+    navLinks.forEach(item => {
+        item.addEventListener('click', () => {
+            wrapperMenu.classList.toggle('open');
+            navMenu.classList.remove('open_nav');
+        })
+    })
 });
