e12247775d54370b1a9d0c4b5deac3f43098ab56
morven2018 <you@example.com>
2025-01-09 23:50:29 +0300

    feat: add some features to GamePage  * add renderKeyPad without style  * add keyboard reading with check to the defined level(alphas and nums)  * add render level and round

--- simon-says/sctripts/simon.js
+++ simon-says/sctripts/simon.js
@@ -1,9 +1,12 @@
 
-const DIGITHS = '0123456789';
-const ALPHAS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const DIGITS = '0123456789';
+const ALPHAS = 'QWERTYUIOP_ASDFGHJKL_ZXCVBNM';
+//const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+let level;
 
 
-function createStartPage(event, level = 'Easy'){
+function renderStartPage(event, level = 'Easy'){
     const startSection = document.createElement('div');
     document.querySelector('body').append(startSection);
     startSection.className = 'start-page';
@@ -62,13 +65,132 @@ function getLevel(level){
 
 function startGame(level){    
     let currLevel = getLevel(level);
+    document.querySelector('.start-page').classList.add("start-page_no-display");
     const hederGame = document.createElement('h2');
     hederGame.textContent = level;
     document.body.append(hederGame);
+    renderGamePage(level, 1);
+}
+
+function renderGamePage(level, round){
+
+
+    renderHeader(level, round);
+
+    clearStartPage();
+
+    renderKeyPad(level);
+
+    const resultArea = document.createElement("div");
+    resultArea.className = "result-area";
+    document.querySelector('body').append(resultArea);
+
+    //
+    //повторить последовательность
+
+    //новая игра(level)
+}
+
+function renderHeader(level, round){
+    const header = document.createElement('header');
+    document.querySelector('body').append(header);
+    header.classList.add("game-header");
+
+    const levelValue = document.createElement("div");
+    levelValue.classList.add('level-of-game');
+    levelValue.value = level;
+    levelValue.textContent = `Level: ${level}`;
+
+    const roundValue = document.createElement("div");
+    roundValue.classList.add('round-of-game');
+    roundValue.textContent = `Round: ${round}`;
+
+    header.append(levelValue);
+    header.append(roundValue);
+
 }
 
 
-function switchLevel(level){
+function renderKeyPad(level){
+    const keyPad = document.createElement('div');
+    document.querySelector('body').append(keyPad);
+    switch(level){
+        case 'Easy':
+            keyPad.classList.add("num-pad");
+            renderNumPad(keyPad);
+            break;
+        case 'Medium':
+            keyPad.classList.add("keyboard");
+            renderKeyboard(keyPad);
+            break;
+        case 'Hard':
+            keyPad.classList.add("full-pad");
+            renderFullKeyboard(keyPad);
+    }
+}
+
+function renderNumPad(keyPad){
+    DIGITS.split("").forEach( (item)  => {
+        const key = document.createElement('button');
+        key.textContent = item;
+        key.setAttribute('id', item);
+        key.setAttribute('value', item);
+        key.classList.add("num-pad-element");
+        keyPad.append(key);
+    });
+}
+
+function renderKeyboard(keyPad){
+    ALPHAS.split("").forEach( (item)  => {
+        const key = document.createElement('button');
+        key.textContent = item;
+        key.setAttribute('id', item);
+        key.setAttribute('value', item);
+        key.classList.add("keyboard-element");
+        keyPad.append(key);
+    });
+}
+
+function renderFullKeyboard(keyPad){
+    const keyboard = document.createElement('div');
+    keyPad.append(keyboard);
+    keyboard.classList = 'full-pad__keys';
+    keyboard.classList.add("keyboard");
+    renderKeyboard(keyboard);
+
+    const numPad = document.createElement('div');
+    keyPad.append(numPad);
+    numPad.classList = 'full-pad__nums';
+    numPad.classList.add("num-pad");
+    renderNumPad(numPad);
+}
+
+
+function getNewKey(level, value){
+    let inputResult = document.querySelector(".result"); 
+    if(!inputResult){
+        inputResult = document.createElement("div");
+        document.querySelector(".result-area").append(inputResult); 
+        inputResult.className = "result";
+        inputResult.textContent = value;
+    } else {
+        const temp = inputResult.textContent;
+        inputResult.textContent = temp + value;
+    }
+}
+
+function isCorrectKey(level, key){
+    switch(level){
+        case 'Easy':
+            return DIGITS.includes(key);
+        case 'Medium':
+            return ALPHAS.includes(key);
+        case 'Hard':
+            return DIGITS.includes(key) || ALPHAS.includes(key);
+    }
+}
+
+/*function switchLevel(level){
     const options = document.querySelectorAll('input[type=radio]');
     options.forEach((radio) => {
         radio.addEventListener('change', () => {
@@ -80,26 +202,27 @@ function switchLevel(level){
     });
     console.log(level);
     return level;
-}
+}*/
 
-window.onload = createStartPage;
-//document.addEventListener("DOMContentLoaded", (event) => createStartPage('Easy'));
+window.onload = renderStartPage;
 
-
-
-//createStartPage('Easy');
 
 const parentElement = document.querySelector('body');
 console.log(parentElement);
+
 parentElement.addEventListener('click', (event) => {
     if (event.target.className === 'start-page__button'){
         startGame(getLevel(level));
         console.log("start-page__button");
     }
-})
-//startButton.onclick = console.log('click');
-//startButton.addEventListener('click', console.log('click'));
+    if (event.target.className === 'num-pad-element' || event.target.className === 'keyboard-element'){
+        console.log(event.target.value);
+        getNewKey(level, event.target.value);
+    }
+});
 
 
-
-
+document.addEventListener('keyup', (event) => {
+  level = document.querySelector('.level-of-game').value;
+  if (isCorrectKey(level, event.key.toLocaleUpperCase())) getNewKey(level, event.key.toLocaleUpperCase());
+});
