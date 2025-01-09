051a22ee3d457a55e4be9d823c99162902d28cdc
morven2018 <you@example.com>
2025-01-09 04:07:17 +0300

    feat: add handler click on generated button

--- simon-says/sctripts/simon.js
+++ simon-says/sctripts/simon.js
@@ -2,10 +2,10 @@
 const DIGITHS = '0123456789';
 const ALPHAS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-function createStartPage(event, level = 'Easy'){
 
+function createStartPage(event, level = 'Easy'){
     const startSection = document.createElement('div');
-    document.body.appendChild(startSection);
+    document.querySelector('body').append(startSection);
     startSection.className = 'start-page';
 
     const startHeader = document.createElement('h1');
@@ -25,6 +25,7 @@ function createStartPage(event, level = 'Easy'){
     headerLegend.textContent = 'Please. Choose level of the game';
     chooseLevel.append(headerLegend);
 
+
     const inputValue = ['Easy', 'Medium', 'Hard'];
 
     inputValue.forEach((item) => {
@@ -34,23 +35,71 @@ function createStartPage(event, level = 'Easy'){
 
         let option = document.createElement('input');
         option.setAttribute('type', 'radio');
-        option.setAttribute('name', item);
+        option.setAttribute('id', item);
+        option.setAttribute('name', 'level');
         option.setAttribute('value', item);
-        if (level == item) option.setAttribute('checked', true);
+        if (item === level) option.setAttribute('checked', true);
 
+        chooseLevel.append(option); 
         chooseLevel.append(optionLabel);
-        chooseLevel.append(option);     
+            
 
     });     
-
     startSection.append(startHeader);
     startSection.append(startButton);
     startSection.append(chooseLevel);
+    
+}
 
+function getLevel(level){
+    const checkedOption = document.querySelector('input:checked');
+    console.log(checkedOption);
+    if (!level) level = 'Easy';
+    if (level !== checkedOption.value) level = checkedOption.value;
+    console.log(level);
+    return level;
+}
 
+function startGame(level){    
+    let currLevel = getLevel(level);
+    const hederGame = document.createElement('h2');
+    hederGame.textContent = level;
+    document.body.append(hederGame);
 }
 
 
+function switchLevel(level){
+    const options = document.querySelectorAll('input[type=radio]');
+    options.forEach((radio) => {
+        radio.addEventListener('change', () => {
+            if (radio.checked){
+                if (radio.value === level) radio.checked = false;
+                else level = radio.value;
+            }
+        })
+    });
+    console.log(level);
+    return level;
+}
+
 window.onload = createStartPage;
+//document.addEventListener("DOMContentLoaded", (event) => createStartPage('Easy'));
+
+
+
+//createStartPage('Easy');
+
+const parentElement = document.querySelector('body');
+console.log(parentElement);
+parentElement.addEventListener('click', (event) => {
+    if (event.target.className === 'start-page__button'){
+        startGame(getLevel(level));
+        console.log("start-page__button");
+    }
+})
+//startButton.onclick = console.log('click');
+//startButton.addEventListener('click', console.log('click'));
+
+
 
 
