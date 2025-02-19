870049703b9b9a23af1b6e60798a5dea666bd118
morven2018 <you@example.com>
2025-02-20 00:03:25 +0300

    refactor: rename *.js to *.ts

--- news-api/src/components/controller/loader.ts
+++ news-api/src/components/controller/loader.ts
@@ -1,11 +1,25 @@
+type categoryType = 'business' | 'entertainment' | 'general' | 'health' | 'science' | 'sports' | 'technology';
+type optionsType = {
+    apiKey?: string;
+    country?: string;
+    category?: categoryType;
+    sources?: string;
+    q?: string;
+    pageSize?: number;
+    page?: number;
+};
+
 class Loader {
-    constructor(baseLink, options) {
+    private baseLink: string | undefined;
+    private options: optionsType;
+
+    constructor(baseLink: string | undefined, options: optionsType) {
         this.baseLink = baseLink;
         this.options = options;
     }
 
     getResp(
-        { endpoint, options = {} },
+        { endpoint, options = {} }: any,
         callback = () => {
             console.error('No callback for GET response');
         }
@@ -13,7 +27,7 @@ class Loader {
         this.load('GET', endpoint, callback, options);
     }
 
-    errorHandler(res) {
+    errorHandler(res: any) {
         if (!res.ok) {
             if (res.status === 401 || res.status === 404)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
@@ -23,7 +37,7 @@ class Loader {
         return res;
     }
 
-    makeUrl(options, endpoint) {
+    makeUrl(options: any, endpoint: any) {
         const urlOptions = { ...this.options, ...options };
         let url = `${this.baseLink}${endpoint}?`;
 
@@ -34,7 +48,7 @@ class Loader {
         return url.slice(0, -1);
     }
 
-    load(method, endpoint, callback, options = {}) {
+    load(method: any, endpoint: any, callback: any, options = {}) {
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
             .then((res) => res.json())
---
+++ news-api/src/components/view/news/news.ts
@@ -0,0 +1,37 @@
+import './news.css';
+
+class News {
+    draw(data: any) {
+        const news = data.length >= 10 ? data.filter((_item: any, idx: number) => idx < 10) : data;
+
+        const fragment = document.createDocumentFragment();
+        const newsItemTemp = document.querySelector('#newsItemTemp')! as HTMLTemplateElement;
+
+        news.forEach((item: any, idx: number) => {
+            const newsClone = newsItemTemp.content.cloneNode(true)! as HTMLElement;
+            if (newsClone) {
+                if (idx % 2) newsClone.querySelector('.news__item')?.classList.add('alt')!;
+                let newsClonePhoto = newsClone.querySelector('.news__meta-photo')! as HTMLElement;
+                newsClonePhoto.style.backgroundImage = `url(${item.urlToImage || 'img/news_placeholder.jpg'})`;
+                newsClone.querySelector('.news__meta-author')!.textContent = item.author || item.source.name;
+                newsClone.querySelector('.news__meta-date')!.textContent = item.publishedAt
+                    .slice(0, 10)
+                    .split('-')
+                    .reverse()
+                    .join('-');
+
+                newsClone.querySelector('.news__description-title')!.textContent = item.title;
+                newsClone.querySelector('.news__description-source')!.textContent = item.source.name;
+                newsClone.querySelector('.news__description-content')!.textContent = item.description;
+                newsClone.querySelector('.news__read-more a')!.setAttribute('href', item.url);
+
+                fragment.append(newsClone);
+            }
+        });
+
+        document.querySelector('.news')!.innerHTML = '';
+        document.querySelector('.news')!.appendChild(fragment);
+    }
+}
+
+export default News;
