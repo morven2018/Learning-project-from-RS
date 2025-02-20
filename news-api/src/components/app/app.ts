676b5ee6f1ef55eec2b1db72c4731c66d4d82e48
morven2018 <you@example.com>
2025-02-20 20:51:09 +0300

    feat: add types to app.ts

--- news-api/src/components/app/app.ts
+++ news-api/src/components/app/app.ts
@@ -1,6 +1,35 @@
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
 
+type dataSourcesType = {
+    status: 'ok' | 'error';
+    sources: Array<sourcesType>;
+};
+type sourcesType = {
+    id: string;
+    name: string;
+    description: string;
+    url: string;
+    category: string;
+    language: string;
+    country: string;
+};
+
+type dataType = { articles: Array<articleType>; status: 'ok' | 'error'; totalResults: number };
+type articleType = {
+    source: {
+        id: string;
+        name: string;
+    };
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string;
+    publishedAt: string;
+    content: string;
+};
+
 class App {
     private controller;
     private view;
@@ -10,12 +39,11 @@ class App {
     }
 
     start() {
-        document
-            .querySelector('.sources')!
-            .addEventListener('click', (e: MouseEvent) =>
-                this.controller.getNews(e, (data: any) => this.view.drawNews(data))
-            );
-        this.controller.getSources((data: any) => this.view.drawSources(data));
+        document.querySelector('.sources')!.addEventListener('click', (e: Event) => {
+            const mEvent = e as MouseEvent;
+            this.controller.getNews(mEvent, (data: dataType): void => this.view.drawNews(data));
+        });
+        this.controller.getSources((data: dataSourcesType): void => this.view.drawSources(data));
     }
 }
 
