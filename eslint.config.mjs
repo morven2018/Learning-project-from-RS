b1f860a388dbd0df6af21148204c50df450335fd
morven2018 <you@example.com>
2025-03-10 02:03:47 +0300

    chore: apply necessary TypeScript  rules in ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -24,6 +24,18 @@ export default [
       "no-console": "warn",
       noInlineConfig: "error",
       reportUnusedDisableDirectives: "error",
+      "@typescript-eslint/consistent-type-assertions": [
+        "error",
+        { assertionStyle: "never" },
+      ],
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/explicit-function-return-type": "error",
+      "@typescript-eslint/explicit-member-accessibility": [
+        "error",
+        { accessibility: "explicit", overrides: { constructors: "off" } },
+      ],
+      "@typescript-eslint/member-ordering": "error",
+      "class-methods-use-this": "error",
     },
   },
   eslintConfigPrettier,
