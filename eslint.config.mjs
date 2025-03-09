d8d41280f29eaa535bb73228385bb306bfdf0139
morven2018 <you@example.com>
2025-03-10 01:57:21 +0300

    chore: enable noInlineConfig and reportUnusedDisableDirectives in ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,29 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import prettier from 'eslint-plugin-prettier';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
     plugins: [
-      'error',
+      "error",
       {
-        endOfLine: 'auto',
+        endOfLine: "auto",
       },
     ],
     rules: {
-      'prettier/prettier': 'error',
-      semi: 'error',
-      'prefer-const': 'error',
-      'no-console': 'warn',
+      "prettier/prettier": "error",
+      semi: "error",
+      "prefer-const": "error",
+      "no-console": "warn",
+      noInlineConfig: "error",
+      reportUnusedDisableDirectives: "error",
     },
   },
   eslintConfigPrettier,
