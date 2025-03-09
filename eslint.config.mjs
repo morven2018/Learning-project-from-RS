791c5bea12160877d61e68eec24f32eb56bd84fb
morven2018 <you@example.com>
2025-03-10 01:50:09 +0300

    chore: add Prettier plugin to ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,28 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    plugins: [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    rules: {
+      'prettier/prettier': 'error',
+      semi: 'error',
+      'prefer-const': 'error',
+      'no-console': 'warn',
+    },
+  },
+  eslintConfigPrettier,
 ];
