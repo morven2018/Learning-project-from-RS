16fa241804d7771e35beede56eaf0d195d3b45ce
morven2018 <you@example.com>
2025-03-10 03:13:07 +0300

    chore: fixed ESLint config to ignore webpack service files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,82 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
+import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import prettier from "eslint-plugin-prettier";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
+  eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    plugins: [
-      "error",
-      {
-        endOfLine: "auto",
+    languageOptions: {
+      globals: {
+        ...globals.browser,
       },
-    ],
+      parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    plugins: {
+      prettier: prettier,
+    },
+
     rules: {
-      "prettier/prettier": "error",
+      "prettier/prettier": [
+        "error",
+        {
+          endOfLine: "auto",
+        },
+      ],
       semi: "error",
       "prefer-const": "error",
       "no-console": "warn",
-      noInlineConfig: "error",
-      reportUnusedDisableDirectives: "error",
+      // "no-inline-config": "error", - устарело далее замена
+      "no-restricted-syntax": [
+        "error",
+        {
+          selector:
+            'Program:has(ExpressionStatement[expression.callee.object.name="eslint"][expression.callee.property.name="disable"] ~ Comment)',
+          message:
+            "Disallow inline ESLint configuration comments.  Use .eslintignore or .eslintrc.js instead.",
+        },
+        {
+          selector:
+            'Program:has(ExpressionStatement[expression.callee.object.name="eslint-disable"][expression.callee.property.name="line"] ~ Comment)',
+          message:
+            "Disallow inline ESLint configuration comments.  Use .eslintignore or .eslintrc.js instead.",
+        },
+        {
+          selector:
+            'Program:has(ExpressionStatement[expression.callee.object.name="eslint-disable"][expression.callee.property.name="next"] ~ Comment)',
+          message:
+            "Disallow inline ESLint configuration comments.  Use .eslintignore or .eslintrc.js instead.",
+        },
+      ],
+      // reportUnusedDisableDirectives: "error",
+    },
+  },
+  {
+    files: ["*.js", "*.mjs", "*.cjs"],
+    languageOptions: {
+      parserOptions: {
+        project: null,
+      },
+    },
+  },
+
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
       "@typescript-eslint/consistent-type-assertions": [
         "error",
         { assertionStyle: "never" },
@@ -38,5 +91,7 @@ export default [
       "class-methods-use-this": "error",
     },
   },
+  { ignores: ["webpack.*.js"] },
+
   eslintConfigPrettier,
 ];
