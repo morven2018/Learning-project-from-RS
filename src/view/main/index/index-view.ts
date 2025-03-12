26d1dbbff79433257b82a1dba00e8024bc2ce1bb
morven2018 <you@example.com>
2025-03-13 00:26:23 +0300

    feat: add  Save JSON button on index page

--- src/view/main/index/index-view.ts
+++ src/view/main/index/index-view.ts
@@ -3,6 +3,7 @@ import { isNotNullable } from '../../../util/is-nullable';
 import ListCreator from '../../../util/list-option/input-field/list-option';
 import type { IElementParameters } from '../../../types/interfaces';
 import type State from '../../../state/state';
+import ListConfigurator from '../../../util/list-configurator/list-configurator';
 
 const CssClasses = {
   INDEX: 'index',
@@ -10,6 +11,9 @@ const CssClasses = {
   BUTTON_ADD_ELEMENT: 'add-list-element__button',
   BUTTON_PASTE_LIST: 'paste-list__button',
   BUTTON_CLEAR_LIST: 'clear-list__button',
+  BUTTON_SAVE: 'save__button',
+  BUTTON_UPLOAD: 'upload__button',
+  BUTTON_START: 'start__button',
 };
 
 const TEXT_CONTENT = {
@@ -18,6 +22,9 @@ const TEXT_CONTENT = {
   BUTTON_ADD_ELEMENT: 'Add Option',
   BUTTON_PASTE_LIST: 'Paste List',
   BUTTON_CLEAR_LIST: 'Clear List',
+  BUTTON_SAVE: 'Save list to the file',
+  BUTTON_UPLOAD: 'Load list from file',
+  BUTTON_START: 'Start',
 };
 
 const PAGE = 'index';
@@ -61,11 +68,42 @@ export default class IndexView extends View {
 
       this.addButton({
         tag: 'button',
-        classNames: [CssClasses.BUTTON_CLEAR_LIST],
-        textContent: TEXT_CONTENT.BUTTON_CLEAR_LIST,
-        callback: (): void => this.list?.clearList(),
+        classNames: [CssClasses.BUTTON_SAVE],
+        textContent: TEXT_CONTENT.BUTTON_SAVE,
+        callback: (): void => this.saveJSON(),
         imageURL: '',
       });
+
+      this.addButton({
+        tag: 'button',
+        classNames: [CssClasses.BUTTON_UPLOAD],
+        textContent: TEXT_CONTENT.BUTTON_UPLOAD,
+        callback: (): void => {},
+        imageURL: '',
+      });
+      this.addButton({
+        tag: 'button',
+        classNames: [CssClasses.BUTTON_START],
+        textContent: TEXT_CONTENT.BUTTON_START,
+        callback: (): void => {},
+        imageURL: '',
+      });
+    }
+  }
+
+  public saveJSON(): void {
+    if (isNotNullable(this.list)) {
+      const content = ListConfigurator.toJSON(
+        this.list.getElements(),
+        this.list?.nextId
+      );
+      const jsonContent = JSON.stringify(content);
+      const blob = new Blob([jsonContent], { type: 'application/json' });
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(blob);
+      link.download = 'option-list.json';
+      link.click();
+      URL.revokeObjectURL(link.href);
     }
   }
 
@@ -75,7 +113,7 @@ export default class IndexView extends View {
       classNames: [CssClasses.LIST_CLASS],
       textContent: '',
     };
-    this.list = new ListCreator(parameters);
+    this.list = new ListCreator(parameters, this.state);
     if (isNotNullable(this.viewElementCreator))
       this.viewElementCreator.addInnerElement(this.list);
   }
