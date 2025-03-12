70b792f5a62db69f0868f69b83616ca312d87fb2
morven2018 <you@example.com>
2025-03-12 03:15:04 +0300

    chore: configure webpack to bundle image files

--- webpack.config.js
+++ webpack.config.js
@@ -45,6 +45,18 @@ const baseConfig = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: 'images/[name].[hash].[ext]',
+            },
+          },
+        ],
+      },
     ],
   },
   resolve: {
