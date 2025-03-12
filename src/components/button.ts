0145ec39c2531d76a7c31e716d5e4830da2f6683
morven2018 <you@example.com>
2025-03-12 04:31:37 +0300

    feat: add buttonCreator class

---
+++ src/components/button.ts
@@ -0,0 +1,8 @@
+import ElementCreator from '../util/element-creator';
+import type { IElementParameters } from '../types/interfaces';
+
+export default class ButtonCreator extends ElementCreator {
+  public createElement(parameters: IElementParameters): void {
+    super.createElement(parameters);
+  }
+}
