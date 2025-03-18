f0dc03b84105eaeaed93615b62f57e038363aa61
morven2018 <you@example.com>
2025-03-18 09:58:59 +0300

    chore: move from folder

---
+++ index.ts
@@ -0,0 +1,3 @@
+import App from './decision-making-tool/src/app';
+
+new App();
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { merge } = require("webpack-merge");
+
+const baseConfig = {
+  entry: {
+    index: "./index.ts",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+      filename: "index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  optimization: {
+    splitChunks: {
+      name: "common",
+      chunks: "all",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.html$/i,
+        use: "html-loader",
+      },
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+              name: "images/[name].[hash].[ext]",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+};
+
+module.exports = ({}, { mode }) => {
+  const isProductionMode = mode === "production";
+  const envConfig = isProductionMode
+    ? require("./webpack.prod.config")
+    : require("./webpack.dev.config");
+
+  return merge(baseConfig, envConfig);
+};
