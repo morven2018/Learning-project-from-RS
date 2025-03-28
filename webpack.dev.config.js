4c0ac3a9f921cd301b9ab8b758043cad8250ccae
morven2018 <you@example.com>
2025-03-28 23:34:01 +0300

    chore: add webpack with basic config

---
+++ webpack.dev.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  target: 'web',
+  devtool: 'inline-source-map',
+  devServer: {
+    open: true,
+    host: 'localhost',
+    port: 4200,
+    hot: true,
+    static: path.resolve(__dirname, './dist'),
+    historyApiFallback: true,
+    // client: { overlay: false },
+  },
+};
