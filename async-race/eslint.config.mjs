0b7cfac4483521bcfcb0d7f131968f7cadd356be
morven2018 <you@example.com>
2025-03-28 23:25:46 +0300

    chore: apply ts with strict mode, prettier and eslint to the project

---
+++ async-race/eslint.config.mjs
@@ -0,0 +1,85 @@
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+// import js from '@eslint/js';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default defineConfig([
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+
+  eslint.configs.recommended,
+  eslintPluginUnicorn.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  // {
+  //   files: ['**/*.{js,mjs,cjs,ts}'],
+  //   plugins: { js },
+  //   extends: ['js/recommended', 'plugin:prettier/recommended'],
+  // },
+  {
+    languageOptions: {
+      globals: globals.builtin,
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ['*.mjs'],
+        },
+        tsconfigRootDir: import.meta.dirname,
+        sourceType: 'module',
+      },
+    },
+  },
+  {
+    extends: ['prettier'],
+    plugins: {
+      prettier: prettier,
+    },
+
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+        },
+      ],
+      semi: 'error',
+      'prefer-const': 'error',
+      'no-console': 'warn',
+
+      '@typescript-eslint/consistent-type-assertions': [
+        'error',
+        { assertionStyle: 'never' },
+      ],
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/explicit-member-accessibility': [
+        'error',
+        { accessibility: 'explicit', overrides: { constructors: 'off' } },
+      ],
+      '@typescript-eslint/member-ordering': 'error',
+
+      'class-methods-use-this': 'error',
+
+      'unicorn/better-regex': 'warn',
+
+      linterOptions: {
+        noInlineConfig: true,
+        reportUnusedDisableDirectives: 'warn',
+      },
+    },
+  },
+  {
+    ignores: ['webpack.*.js', 'node_modules/', 'dist/', 'build/'],
+  },
+
+  {
+    files: ['eslint.config.mjs'],
+    languageOptions: {
+      parserOptions: {
+        sourceType: 'module',
+      },
+    },
+  },
+
+  eslintConfigPrettier,
+]);
