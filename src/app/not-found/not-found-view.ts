1f06aca836091abf9b505daf511acee8c3f7c510
morven2018 <you@example.com>
2025-03-29 04:20:48 +0300

    feat: add 404 page

---
+++ src/app/not-found/not-found-view.ts
@@ -0,0 +1,77 @@
+import View from '../../view';
+
+import type Router from '../../../router/router';
+import type { IView } from '../../../types/interfaces';
+
+import './not-found.scss';
+import notFoundImage from '../../../../asserts/404.jpg';
+import backIcon from '../../../../asserts/icons/back.png';
+import homeIcon from '../../../../asserts/icons/home.png';
+
+const CssClasses = {
+  INDEX: 'not-found',
+  IMAGE: 'not-found__image',
+  TEXT: 'not-found__text',
+  BUTTON: 'not-found__button',
+};
+
+const IMAGE_URL = notFoundImage.toString();
+const BACK_URL = backIcon.toString();
+const HOME_URL = homeIcon.toString();
+
+const TEXT_CONTENT = {
+  TITLE: 'Page is not founded',
+  BACK: 'Back',
+  HOME: 'Home',
+};
+
+export default class NotFoundView extends View implements IView {
+  private router: Router;
+
+  constructor(router: Router) {
+    const parameters = {
+      tag: 'section',
+      classNames: [CssClasses.INDEX],
+      imageURL: IMAGE_URL,
+      imageAlt: '404 Not Found',
+    };
+    super(parameters);
+    this.router = router;
+    this.configureView();
+  }
+
+  public configureView(): void {
+    this.addImage({
+      tag: 'img',
+      classNames: [CssClasses.IMAGE],
+      textContent: '',
+      imageURL: IMAGE_URL,
+    });
+
+    this.addTextMessage({
+      tag: 'p',
+      classNames: [CssClasses.TEXT],
+      textContent: TEXT_CONTENT.TITLE,
+    });
+
+    this.addButton({
+      tag: 'button',
+      classNames: [CssClasses.BUTTON, 'back'],
+      textContent: '',
+      title: TEXT_CONTENT.BACK,
+      callback: (): void => globalThis.history.back(),
+      imageURL: BACK_URL,
+    });
+
+    this.addButton({
+      tag: 'button',
+      classNames: [CssClasses.BUTTON, 'to-home-page'],
+      textContent: '',
+      title: TEXT_CONTENT.HOME,
+      callback: (): void => {
+        this.router.navigateTo('#/index');
+      },
+      imageURL: HOME_URL,
+    });
+  }
+}
---
+++ src/lib/state/state.ts
@@ -0,0 +1,140 @@
+import type { IListCreator, IState, IValueList } from '../types/interfaces';
+
+const KEY_FOR_SAVE_LIST = 'optionList';
+export default class State implements IState {
+  public fields: Map<string, string>;
+  private listCreator: IListCreator | undefined;
+
+  constructor() {
+    this.fields = State.loadState();
+  }
+
+  public static loadState(): Map<string, string> {
+    const storageItem = localStorage.getItem(KEY_FOR_SAVE_LIST);
+
+    if (storageItem) {
+      try {
+        const parsed: unknown = JSON.parse(storageItem);
+        if (typeof parsed === 'object' && State.isFieldsObject(parsed)) {
+          return new Map(Object.entries(parsed));
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    return new Map<string, string>();
+  }
+
+  public static isFieldsObject(data: unknown): data is Record<string, string> {
+    if (!data) {
+      return false;
+    }
+
+    for (const key in data) {
+      if (typeof key === 'string' || typeof data[key] !== 'string') {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static loadFromLocalStorage():
+    | { list: HTMLElement[]; lastId: number }
+    | undefined {
+    const savedList = localStorage.getItem(KEY_FOR_SAVE_LIST);
+    if (savedList) {
+      try {
+        const jsonData: unknown = JSON.parse(savedList);
+        return ListConfigurator.fromJSON(jsonData);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    return undefined;
+  }
+
+  public static getOptionList(): IValueList | undefined {
+    const savedData = this.loadFromLocalStorage();
+    if (savedData) {
+      const valueList: IValueList = {};
+      for (const element of savedData.list) {
+        if (ListConfigurator.isIElementInfo(element) && element.title) {
+          valueList[element.title] = Number.parseInt(element.weight);
+        }
+      }
+      return valueList;
+    }
+    return undefined;
+  }
+
+  public static saveToLocalStorage(
+    elementList: HTMLElement[],
+    lastId: number
+  ): void {
+    if (elementList && Array.isArray(elementList)) {
+      const storedElements = [
+        ...new Set(elementList.map((element) => element.getAttribute('id'))),
+      ]
+        .map((id) =>
+          elementList.find((element) => element.getAttribute('id') === id)
+        )
+        .filter((element) => element);
+
+      localStorage.removeItem(KEY_FOR_SAVE_LIST);
+
+      const content = ListConfigurator.toJSON(storedElements, lastId);
+
+      const jsonContent = JSON.stringify(content);
+
+      localStorage.setItem(KEY_FOR_SAVE_LIST, jsonContent);
+    }
+  }
+
+  public static shuffleObject<T>(object: Record<string, T>): Record<string, T> {
+    const entries = Object.entries(object);
+
+    for (let index = entries.length - 1; index > 0; index--) {
+      const index_ = Math.floor(Math.random() * (index + 1));
+      [entries[index], entries[index_]] = [entries[index_], entries[index]];
+    }
+
+    return Object.fromEntries(entries);
+  }
+
+  public setListCreator(listCreator: IListCreator): void {
+    this.listCreator = listCreator;
+  }
+
+  public getElements(): HTMLElement[] | undefined {
+    return this.listCreator?.elements;
+  }
+
+  public getNextId(): number | undefined {
+    return this.listCreator?.nextId;
+  }
+
+  public saveState(): void {
+    const fieldsObject = Object.fromEntries(this.fields.entries());
+
+    localStorage.setItem(KEY_FOR_SAVE_LIST, JSON.stringify(fieldsObject));
+
+    if (this.listCreator) {
+      State.saveToLocalStorage(
+        this.listCreator.elements,
+        this.listCreator.nextId
+      );
+    }
+  }
+
+  public setField(name: string, value: string): void {
+    this.fields.set(name, value);
+  }
+
+  public getField(name: string): string {
+    if (this.fields.has(name)) {
+      const result = this.fields.get(name);
+      if (typeof result === 'string') return result;
+    }
+    return '';
+  }
+}
