80f04709d8ca6341ac5194c7c2b32f7e4a6f15cb
morven2018 <you@example.com>
2025-04-15 22:22:32 +0300

    feat: add elementCreator

---
+++ src/components/element-creator.ts
@@ -0,0 +1,63 @@
+import { IElementCreator, IElementParameters } from '../lib/types/interfaces';
+import type { CallbackType } from '../lib/types/types';
+
+export default class ElementCreator implements IElementCreator {
+  public element: HTMLElement | undefined;
+
+  constructor(parameters: IElementParameters) {
+    this.element = undefined;
+    this.createElement(parameters);
+  }
+
+  public getElement(): HTMLElement | undefined {
+    if (this.element) return this.element;
+    return undefined;
+  }
+
+  public addInnerElement(element: HTMLElement | IElementCreator): void {
+    if (element instanceof ElementCreator) {
+      const oneElement = element.getElement();
+      if (oneElement && this.element) this.element.append(oneElement);
+    } else if (element instanceof HTMLElement && this.element)
+      this.element.append(element);
+  }
+
+  public createElement(parameters: IElementParameters): void {
+    this.element = document.createElement(parameters.tag);
+    this.setCssClasses(parameters.classNames);
+    this.setTextContent(parameters.textContent);
+    if (parameters.callback) this.setCallback(parameters.callback);
+  }
+
+  public setCssClasses(cssClasses: Array<string> = []): void {
+    if (this.element)
+      cssClasses.map((cssClass) => this.element?.classList.add(cssClass));
+  }
+
+  public setTextContent(text: string = ''): void {
+    if (this.element) this.element.textContent = text;
+  }
+
+  public setCallback(callback: CallbackType): void {
+    if (this.element && callback && typeof callback === 'function')
+      this.element.addEventListener('click', (event) => callback(event));
+  }
+
+  public clearInnerElements(): void {
+    if (this.element instanceof HTMLElement) {
+      this.element.replaceChildren();
+    }
+  }
+  public update(parameters: {
+    tag?: string;
+    classNames?: string[];
+    textContent?: string;
+  }): void {
+    if (parameters.classNames && this.element) {
+      this.element.className = parameters.classNames.join(' ');
+    }
+    if (parameters.textContent && this.element) {
+      this.element.textContent = parameters.textContent;
+    }
+  }
+}
